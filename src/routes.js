c6399c741729753c90c34f009ce3edb7754effd5
ianrscosta <ianrcosta@gmail.com>
2020-06-13 18:27:56 -0300

    Users CRUD done

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ const routes = express.Router();
 const usersController = require('./controllers/usersController');
 
 routes.get('/users', usersController.index);
+routes.post('/users', usersController.create);
+routes.put('/users/:id', usersController.update);
+routes.delete('/users/:id', usersController.delete);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
+app.use((req, res, next) => {
+    const error = new Error('not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(3001, () => console.log('server is running'));
