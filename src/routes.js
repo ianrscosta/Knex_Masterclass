be296a0ce2bb744b71186f5e2748010074c23e9b
ianrscosta <ianrcosta@gmail.com>
2020-06-13 15:45:16 -0300

    using knex

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const usersController = require('./controllers/usersController');
+
+routes.get('/users', usersController.index);
+
+module.exports = routes;
